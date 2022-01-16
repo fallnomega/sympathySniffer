8cc0acdb1a4a78d44d1c7e4a749f1d47d6234565
Jason Hillin <jasonhillin@usscmlap086953.gateway.sonic.net>
2022-01-16 11:31:20 -0800

    added missed info, bulk tickers to check, cleaned up ouput

--- tgtHitEventually.py
+++ tgtHitEventually.py
@@ -1,120 +1,179 @@
-#to see if picks eventually hit based on choices you or others took in the past
+# to see if picks eventually hit based on choices you or others took in the past
 import datetime
 import os
 import yfinance as yf
 import pandas as pd
+
 pd.options.mode.chained_assignment = None  # default='warn'
 
-from collections import Counter
-import csv
-import glob
-import operator
-import openpyxl
 
-#define how one specifies the ticker to look up plus price target.
-#get ticker data
-#pull date, current price, dates high, and dates low info from data
+# pull date, current price, dates high, and dates low info from data
 # data result example calling for AAPL:
-#|Date|Adj Close|Close|High||Low|Open|Volume|Symbol
-#|1/15/20|76.69576263|77.83499908|78.875|77.38749695|77.96250153|121923600|AAPL
-#|1/16/20|77.65648651|78.80999756|78.92500305|78.02249908|78.39749908|108829200|AAPL
-
-def get_ticker_data(tickers,directory):
+# |Date|Adj Close|Close|High||Low|Open|Volume|Symbol
+# |1/15/20|76.69576263|77.83499908|78.875|77.38749695|77.96250153|121923600|AAPL
+# |1/16/20|77.65648651|78.80999756|78.92500305|78.02249908|78.39749908|108829200|AAPL
+def get_ticker_data(tickers, directoryz):
     for ind in tickers.index:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        if not os.path.exists('{}/{}.csv'.format(directory,tickers['Symbol'][ind])):
+        if not os.path.exists(directoryz):
+            os.makedirs(directoryz)
+        if not os.path.exists('{}/{}.csv'.format(directoryz, tickers['Symbol'][ind])):
             try:
 
                 print('Getting data for: {}'.format(tickers['Symbol'][ind]))
                 # print price_target
-                df = yf.download(tickers['Symbol'][ind] , period='2y', interval='1d')
+                df = yf.download(tickers['Symbol'][ind], period='2y', interval='1d')
                 df.dropna(inplace=True)
                 df["Symbol"] = tickers['Symbol'][ind]
-                df.to_csv('{}/{}.csv'.format(directory,tickers['Symbol'][ind]))
+                df.to_csv('{}/{}.csv'.format(directoryz, tickers['Symbol'][ind]))
 
             except Exception as ex:
                 print('Error:', ex)
         else:
             print('-> Already have {}.csv file so skipping'.format(tickers['Symbol'][ind]))
 
-#determine if short or long
-#long - determine if it ever hit the PT
-def longTargethit(tickers,directory):
+
+# determine if short or long
+def long_or_short():
+    pass
+
+
+# long - determine if it ever hit the PT ((entry - stop) * 2) + entry
+def long_target_hit(tickers, directoryz):
     hits = 0
+    ticker_hits = []
+    ticker_misses = []
     for ind in tickers.index:
-        print ('Opening {}.csv'.format(tickers['Symbol'][ind]))
-        print('Parsing data to see if {} ever hit ${}'.format(tickers['Symbol'][ind],tickers['Target'][ind]))
-        data = pd.read_csv('{}/{}.csv'.format(directory,tickers['Symbol'][ind]))
+        # print ('Opening {}.csv'.format(tickers['Symbol'][ind]))
+        # print('Parsing data to see if {} ever hit ${}'.format(tickers['Symbol'][ind],tickers['Target'][ind]))
+        data = pd.read_csv('{}/{}.csv'.format(directoryz, tickers['Symbol'][ind]))
         tickers['Date'][ind] = pd.to_datetime(tickers['Date'][ind], errors='coerce')
 
         for indy in data.index:
             data['Date'][indy] = pd.to_datetime(data['Date'][indy], errors='coerce')
 
             if data['High'][indy] >= tickers['Target'][ind] and data['Date'][indy] >= tickers['Date'][ind]:
-                print ('''
-                Ticker = {}
-                Entry Date = {}
-                PT = {}
-                PT hit on {} and had a high of {}
-                '''.format(data['Symbol'][indy],tickers['Date'][ind],tickers['Target'][ind],data['Date'][indy],data['High'][indy]))
-                hits = hits+1
+                print ('Ticker = {} -> Entry Date = {} -> PT = {} -> PT hit on {} and had a high of {}'
+                       .format(data['Symbol'][indy], tickers['Date'][ind], tickers['Target'][ind], data['Date'][indy],
+                               data['High'][indy]))
+                hits = hits + 1
+                ticker_hits.append(data['Symbol'][indy])
                 break
             continue
 
-        print ('hits total = {} \n out of a total of {} ideas.\n\n'.format(hits,len(tickers)))
+        ticker_misses = [item for item in tickers['Symbol'] if item not in ticker_hits]
+    print ('\n\nHits total = {} \nOut of a total of {} ideas.'.format(hits, len(tickers)))
+    print ('Tickers that hit: {}'.format(ticker_hits))
+    print ('Tickers that missed: {}'.format(ticker_misses))
 
     return 0
 
-#short - determine if it ever hit the PT
-# def shortTargethit(ticker):
-#     print('Looking to see if there are sympathy plays for: {}'.format(ticker))
-#     df = pd.read_excel('sector_final_list.xlsx')
-#     target = df[(df['Symbol'] == ticker)]
-#     target_list = df.values.tolist()
-#     sector = target['Sector'].values[0]
-#     # print (sector)
-#     list_dates = target.values.tolist()
-#     same_list_results = list()
-#     diff_sector_list = list()
-#
-#     for row in list_dates:
-#         # print (row[3])
-#         for row_df in target_list:
-#             if row[3] == row_df[3] and sector == row_df[9]:
-#                 # print ('SAME SECTOR - Date: {} | Symbol: ${} | Volume: {} | Open vs HOD percent difference: {} %'.format(row_df[3],row_df[10],row_df[11], round(row_df[8], 2)))
-#                 same_list_results.append(row_df[10])
-#     # print (list_results)
-#     same_sector_count = Counter(same_list_results)
-#     print ("Same Sector count:\n {}\n\n".format(same_sector_count))
-#
-#     for row in list_dates:
-#         for row_df in target_list:
-#             if row[3] == row_df[3]:
-#                 # print ('CROSS SECTOR - Date: {} | Symbol: ${} | Volume: {} | Open vs HOD percent difference: {} % | Sector: {}'.format(row_df_dif_sector[3],row_df_dif_sector[10],row_df_dif_sector[11], round(row_df_dif_sector[8], 2),row_df_dif_sector[9]))
-#                 diff_sector_list.append(row_df[10])
-#     diff_sector_count = Counter(diff_sector_list)
-#
-#     print("Check cross sector potential sympathy plays :\n {}".format(diff_sector_count))
-#
-#
-#
-#
-#
-#     return 0
+
+# short - determine if it ever hit the PT ((stop -  entry) * 2) - entry
+def short_target_hit(tickers, directoryz):
+    pass
 
 
+# TESTING VARs AND WHAT NOT
+# ticker_info = {'Symbol': ['AAL', 'VISL', 'SHWZ', 'AMD'],
+#       'Target': [20, 1000, 1000, 91.97],
+#        'Date': [datetime.datetime(2021,01,15), datetime.datetime(2021,01,15),
+#                 datetime.datetime(2021,01,19), datetime.datetime(2021,02,04)]}
 
-ticker_pt = {'AAL':20, 'VISL':2.3, 'SHWZ':2.5, 'AMD':91.76, 'ADMP':2, 'azrx':2, 'pltr':40, 'tsla':870, 'AIKI':2.62, 'AESE':3.5, 'ELYS':9.49, 'SPY':400, 'AAL':10.11, 'NIO':70, 'AAPL':145, 'AMC':16, 'AMC':17, 'NIO':60, 'MCD':210, 'FB':270, 'AEP':90, 'SNOW':300, 'DKNG':70, 'spce':55, 'spce':58, 'amzn':3500, 'nndm':20, 'ZM':400, 'DVAX':6.44, 'LODE':4, 'PHUN':4, 'INAQ':24.95, 'AACG':6, 'NFLX':560, 'AMD':90, 'JNJ':170, 'spce':60, 'nio':65, 'dkng':65, 'xpev':53, 'uber':65, 'ko':55, 'DIS':189.39, 'TTWO':214.39, 'SQ':264.69, 'ZAGG':5.5, 'LANC':230, 'sndl':2, 'ENBL':6.15, 'gsat':2.1, 'spce':60, 'IGC':3.1, 'EXPR':3.27, 'NXTD':3.4, 'SPCE':57.5, 'nxtd':3, 'pltr':40, 'MYT':4, 'ATIF':1.56, 'DPW':7.85, 'glw':39.39, 'nio':63.1, 'sckt':12.167, 'yell':9, 'CBAT':9.5, 'CBAT':9, 'MYT':7, 'SOLO':9.05, 'TLRY':29.61, 'GE':12.9, 'CLSN':3.5, 'AAL':21.58, 'YELL':9, 'U':106, 'OCGN':9.82, 'WKHS':18, 'NVDA':600, 'NTCO':20, 'CEF':20, 'SCKT':16.09, 'EBON':10.4, 'AQUA':28.62, 'SOS':9.99, 'XTNT':4, 'DKNG':75, 'WMT':133, 'SOS':9, 'CHFS':11.8, 'HTBX':9.99, 'LYFT':65, 'OCGN':12.21, 'LYFT':61.8, 'BRQS':2, 'PBR':8.15, 'MAC':15.89, 'SPXU':31, 'PBR':8.15, 'BAC':39.84, 'SQQQ':20, 'QQQ':315, 'SPY':400, 'lyft':70, 'WMT':135, 'COST':324, 'XPEV':40, 'SRNE':9.75, 'T':32.5, 'IBM':135, 'PTON':25, 'BLDP':114, 'LFMD':18.83, 'BAC':235, 'LFMD':23, 'XELA':5, 'QS':55, 'BAC':240, 'SNAP':62.56, 'PYPL':264.54, 'GTT':3.06, 'ENVB':8, 'AMC':13.5, 'ENTX':6.1, 'JMIA':4, 'MARA':55.32, 'AYX':93.91, 'DFEN':42.05, 'DOCU':286.32, 'LFMD':23, 'TLRY':40, 'K':65, 'TLT':150, 'TLT':129.68, 'cidm':2.61, 'cigx':8.1, 'JUVAF':1.2, 'REI':1, 'STT':15, 'T':33, 'TLT':2, 'U':118, 'SEAC':2.08, 'TSIA':12.48, 'WKEY':21, 'X':3.2, 'DISCA':47.5196, 'NIO':43.06, 'PFMT':2.6, 'YVR':5.02, 'GDDY':90, 'NPA':15, 'RIOT':60, 'FTFT':9.39, 'VIAC':45, 'RAIL':9.32, 'PMFT':2.61, 'PHUN':2.66, 'PAVM':8, 'sfet':2, 'VERY':25, 'AFMD':10.29, 'FEYE':25, 'TENX':2.39, 'MDT':130, 'pbts':3.19, 'CVS':90, 'CVS':88, 'KYMR':70, 'CIDM':2.1, 'ADMP':1.5, 'HUT':5.91, 'DPRO':4.5, 'DPRO':4, 'VXRT':11, 'SOFI':20, 'AUPH':16, 'ARRY':23, 'PLTR':28, 'GNLN':3.6, 'DPRO':6, 'VXRT':10.5, 'COIN':304.99, 'AYX':90, 'AUPH':12.88, 'ARRY':23, 'LTCH':11.75, 'BTBT':20, 'SRPT':100, 'OGI':3.45, 'ABNB':165, 'BTBT':20, 'SQQQ':9, 'SLV':30, 'T':31, 'CTRM':5, 'SLV':30, 'RIDE':8.5, 'LQTM':0.275, 'ATIF':8, 'MDIA':12.95, 'LOW':220, 'NURO':20, 'TGT':4, 'IPOF':11.15,}
 directory = 'targetHitEventually'
-ticker_info = {'Symbol': ['AAL', 'VISL', 'SHWZ', 'AMD'],
-        'Target': [20, 2.3, 2.5, 91.97],
-        'Date': [datetime.datetime(2021,01,15), datetime.datetime(2021,01,15), datetime.datetime(2021,01,19), datetime.datetime(2021,02,04)]}
-ticker_df = pd.DataFrame(ticker_info, columns=['Symbol','Target','Date'])
 
-get_ticker_data(ticker_df,directory)
-longTargethit(ticker_df,directory)
+# LONGS
+ticker_info = {'Symbol': ['AUPH', 'MBIO', 'AEP', 'SHWZ', 'ELYS', 'VLNCF', 'ZAGG', 'YELL', 'AQUA', 'SOS', 'T', 'IBM',
+                          'MILE', 'LFMD', 'SNAP', 'PYPL', 'K', 'TLRY', 'TLT', 'TLT', 'GDDY', 'JUVAF', 'T', 'NPA',
+                          'TSIA', 'NIO', 'CLVS', 'U', 'FEYE', 'MDT', 'DASH', 'SH', 'ASTS', 'CVM', 'JOB', 'NET',
+                          'FEYE', 'DFLYF', 'SBRA', 'VIAC', 'KALV', 'PRQR', 'ACAD', 'GEO', 'REI', 'DASH', 'SIEN',
+                          'GPRO', 'GBTC', 'ARVL', 'BLUE', 'GSAT', 'DNN', 'TLLTF', 'GPRO', 'WISH', 'BBIG', 'ETHE',
+                          'CIEN', 'BKI', 'AYX', 'LULU', 'SOFI', 'GNLN', 'GPRO', 'YALA', 'HUT', 'GNLN', 'POLA',
+                          'SNPR', 'SFTW', 'DFLYF', 'BABA', 'WISH', 'PENN', 'SUMO', 'FXI', 'BBIG', 'GNLN', 'ETHE',
+                          'DFLYF', 'CVS', 'HUT', 'KYMR', 'CIDM', 'ADMP', 'VXRT', 'SOFI', 'DPRO', 'AUPH', 'PLTR',
+                          'ARRY', 'GNLN', 'DPRO', 'IGT', 'BTBT', 'VXRT', 'COIN', 'AYX', 'AUPH', 'ARRY', 'LTCH',
+                          'SRPT', 'OGI', 'ABNB', 'BTBT', 'SQQQ', 'SLV', 'T', 'REFR', 'CTRM', 'SLV', 'LQMT', 'ATIF',
+                          'RIDE', 'NURO', 'LOW', 'MDIA', 'RWLK', 'REI', 'BLUE', 'IPOF', 'BBKCF', 'OSCR', 'MAPS',
+                          'FIZZ', 'SOFI', 'VERB', 'HOOD', 'QS', 'CBAY', 'PENN', 'TRIP', 'DPRO', 'GME', 'JKS', 'BTBT',
+                          'MNKD', 'UAL', 'IRNT', 'FUBO', 'PSLV', 'BBBY', 'BKKT', 'GLD', 'BKD', 'ZKIN', 'BITF', 'XSPA'],
+               'Target': [14.02, 5.22, 84.38, 2.36, 8.41, 2.23, 4.68, 7.82, 27.25, 8.31, 32.18, 135.56,
+                          11.96, 22.22, 60.79, 259.22, 63.90, 39.78, 139.04, 144.44, 81.21, 1.06, 32.54,
+                          15.44, 11.99, 40.84, 8.40, 117.32, 20.22, 126.26, 147.16, 17.15, 9.70, 19.42,
+                          0.70, 80.96, 20.92, 1.81, 18.22, 46.80, 30.40, 7.60, 24.52, 6.80, 2.94, 162.22,
+                          8.46, 10.86, 39.79, 23.52, 36.60, 1.94, 1.62, 0.70, 13.68, 15.13, 6.39, 18.92,
+                          59.24, 76.26, 95.72, 366.82, 19.02, 5.00, 12.93, 22.00, 6.64, 5.21, 12.62, 10.32,
+                          10.24, 1.55, 203.45, 13.12, 75.80, 24.06, 46.41, 4.66, 4.62, 17.54, 1.76, 86.45,
+                          5.84, 69.24, 2.08, 1.27, 9.58, 19.55, 4.67, 16.92, 28.14, 22.56, 3.00, 3.39,
+                          19.14, 16.64, 9.66, 294.34, 85.34, 15.88, 22.18, 12.22, 87.62, 3.23, 166.44,
+                          16.98, 8.04, 24.14, 28.93, 2.99, 3.04, 23.63, 0.14, 5.32, 8.20, 16.23, 211.20,
+                          11.32, 3.37, 3.58, 21.62, 10.78, 1.44, 20.80, 17.34, 56.90, 18.78, 2.74,
+                          47.74, 26.10, 4.70, 88.32, 41.32, 5.26, 230.20, 62.72, 15.50, 5.19, 51.84,
+                          16.88, 37.00, 9.25, 24.58, 28.60, 186.38, 7.15, 3.40, 9.60, 1.98],
+               'Date': [datetime.datetime(2021, 1, 8), datetime.datetime(2021, 1, 11), datetime.datetime(2021, 1, 15),
+                        datetime.datetime(2021, 1, 19), datetime.datetime(2021, 1, 27), datetime.datetime(2021, 1, 28),
+                        datetime.datetime(2021, 2, 8), datetime.datetime(2021, 2, 16), datetime.datetime(2021, 2, 25),
+                        datetime.datetime(2021, 2, 25), datetime.datetime(2021, 3, 8), datetime.datetime(2021, 3, 8),
+                        datetime.datetime(2021, 3, 8), datetime.datetime(2021, 3, 9), datetime.datetime(2021, 3, 10),
+                        datetime.datetime(2021, 3, 10), datetime.datetime(2021, 3, 16), datetime.datetime(2021, 3, 16),
+                        datetime.datetime(2021, 3, 17), datetime.datetime(2021, 3, 19), datetime.datetime(2021, 3, 26),
+                        datetime.datetime(2021, 3, 26), datetime.datetime(2021, 3, 29), datetime.datetime(2021, 3, 29),
+                        datetime.datetime(2021, 3, 29), datetime.datetime(2021, 3, 30), datetime.datetime(2021, 3, 31),
+                        datetime.datetime(2021, 3, 30), datetime.datetime(2021, 4, 5), datetime.datetime(2021, 4, 12),
+                        datetime.datetime(2021, 4, 12), datetime.datetime(2021, 4, 15), datetime.datetime(2021, 4, 20),
+                        datetime.datetime(2021, 4, 20), datetime.datetime(2021, 4, 21), datetime.datetime(2021, 4, 22),
+                        datetime.datetime(2021, 4, 23), datetime.datetime(2021, 4, 26), datetime.datetime(2021, 5, 7),
+                        datetime.datetime(2021, 5, 12), datetime.datetime(2021, 5, 17), datetime.datetime(2021, 5, 17),
+                        datetime.datetime(2021, 5, 17), datetime.datetime(2021, 5, 17), datetime.datetime(2021, 5, 18),
+                        datetime.datetime(2021, 5, 24), datetime.datetime(2021, 5, 24), datetime.datetime(2021, 5, 24),
+                        datetime.datetime(2021, 5, 26), datetime.datetime(2021, 5, 26), datetime.datetime(2021, 5, 28),
+                        datetime.datetime(2021, 6, 1), datetime.datetime(2021, 6, 1), datetime.datetime(2021, 6, 9),
+                        datetime.datetime(2021, 6, 11), datetime.datetime(2021, 6, 15), datetime.datetime(2021, 6, 22),
+                        datetime.datetime(2021, 6, 22), datetime.datetime(2021, 6, 22), datetime.datetime(2021, 6, 21),
+                        datetime.datetime(2021, 6, 22), datetime.datetime(2021, 6, 21), datetime.datetime(2021, 6, 28),
+                        datetime.datetime(2021, 6, 29), datetime.datetime(2021, 6, 30), datetime.datetime(2021, 7, 1),
+                        datetime.datetime(2021, 7, 2), datetime.datetime(2021, 7, 1), datetime.datetime(2021, 7, 6),
+                        datetime.datetime(2021, 7, 6), datetime.datetime(2021, 7, 6), datetime.datetime(2021, 7, 7),
+                        datetime.datetime(2021, 7, 8), datetime.datetime(2021, 7, 9), datetime.datetime(2021, 7, 12),
+                        datetime.datetime(2021, 7, 12), datetime.datetime(2021, 7, 12), datetime.datetime(2021, 7, 12),
+                        datetime.datetime(2021, 7, 13), datetime.datetime(2021, 7, 20), datetime.datetime(2021, 7, 23),
+                        datetime.datetime(2021, 7, 23), datetime.datetime(2021, 8, 2), datetime.datetime(2021, 8, 2),
+                        datetime.datetime(2021, 8, 2), datetime.datetime(2021, 8, 6), datetime.datetime(2021, 8, 6),
+                        datetime.datetime(2021, 8, 6), datetime.datetime(2021, 8, 4), datetime.datetime(2021, 8, 9),
+                        datetime.datetime(2021, 8, 18), datetime.datetime(2021, 8, 17), datetime.datetime(2021, 8, 17),
+                        datetime.datetime(2021, 8, 18), datetime.datetime(2021, 8, 23), datetime.datetime(2021, 8, 23),
+                        datetime.datetime(2021, 8, 23), datetime.datetime(2021, 8, 23), datetime.datetime(2021, 8, 23),
+                        datetime.datetime(2021, 8, 23), datetime.datetime(2021, 8, 23), datetime.datetime(2021, 8, 23),
+                        datetime.datetime(2021, 8, 25), datetime.datetime(2021, 8, 31), datetime.datetime(2021, 8, 30),
+                        datetime.datetime(2021, 8, 30), datetime.datetime(2021, 9, 1), datetime.datetime(2021, 9, 2),
+                        datetime.datetime(2021, 9, 2), datetime.datetime(2021, 9, 13), datetime.datetime(2021, 9, 15),
+                        datetime.datetime(2021, 9, 15), datetime.datetime(2021, 9, 20), datetime.datetime(2021, 9, 20),
+                        datetime.datetime(2021, 9, 19), datetime.datetime(2021, 9, 22), datetime.datetime(2021, 9, 21),
+                        datetime.datetime(2021, 9, 21), datetime.datetime(2021, 9, 22), datetime.datetime(2021, 9, 27),
+                        datetime.datetime(2021, 9, 27), datetime.datetime(2021, 9, 28), datetime.datetime(2021, 9, 30),
+                        datetime.datetime(2021, 9, 30), datetime.datetime(2021, 10, 4), datetime.datetime(2021, 10, 5),
+                        datetime.datetime(2021, 10, 7), datetime.datetime(2021, 10, 7), datetime.datetime(2021, 10, 11),
+                        datetime.datetime(2021, 10, 11), datetime.datetime(2021, 10, 12),
+                        datetime.datetime(2021, 10, 15),
+                        datetime.datetime(2021, 10, 15), datetime.datetime(2021, 10, 20),
+                        datetime.datetime(2021, 10, 19),
+                        datetime.datetime(2021, 10, 19), datetime.datetime(2021, 10, 20),
+                        datetime.datetime(2021, 10, 22),
+                        datetime.datetime(2021, 10, 26), datetime.datetime(2021, 10, 29),
+                        datetime.datetime(2021, 11, 1),
+                        datetime.datetime(2021, 11, 8), datetime.datetime(2021, 11, 11),
+                        datetime.datetime(2021, 11, 12),
+                        datetime.datetime(2021, 11, 17), datetime.datetime(2021, 11, 18),
+                        datetime.datetime(2021, 11, 17),
+                        datetime.datetime(2021, 1, 23), datetime.datetime(2021, 1, 24)]}
+# Pandas engaged
+ticker_df = pd.DataFrame(ticker_info, columns=['Symbol', 'Target', 'Date'])
+# get ticker info from yfinance
+get_ticker_data(ticker_df, directory)
+# check for what longs hit the PT eventually
+long_target_hit(ticker_df, directory)
+short_target_hit(ticker_df, directory)
 
-# print ticker_pt['ticker']
-# print ticker_pt['price_target']
-# longTargethit(ticker_pt,'targetHitEventually')
+# To do -
+# add way to manually add tickers,pt,date so it isnt hard coded in.
+# implement the short scan
+# define how one specifies the ticker to look up plus price target.
+exit()
